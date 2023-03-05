7c7000fa5ec9a69ed3113df59d2a175d17aae98c
Mika Ahopelto <mika.ahopelto@gmail.com>
2023-03-05 15:18:16 +0200

    Draft front page layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Future of Work and Life';
+export const SITE_DESCRIPTION = 'My Thoughts About The Future Of Work, Technology, Life, Environment And The Balance Between All Of These';
