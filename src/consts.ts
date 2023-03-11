4f36e4bffe661a7f4dac2a70672fab2aeafa1e3f
Mika Ahopelto <mika.ahopelto@gmail.com>
2023-03-11 17:53:28 +0200

    Fixed blog name and descriptive texts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Future of Work and Life';
-export const SITE_DESCRIPTION = 'My Thoughts About The Future Of Work, Technology, Life, Environment And The Balance Between All Of These';
+export const SITE_TITLE = "Hackin' and Rollin'";
+export const SITE_DESCRIPTION = 'Thoughts, Ideas and Findings on a Journey to Solve Meaningful Challenges With the Help of Technology';
+export const ABOUT_ME = 'Tech explorer, outdoor, bjj and family man. Continuously searching for meaningful questions and innovative solutions to them.'
