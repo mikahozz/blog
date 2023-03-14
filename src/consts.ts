aea11a53975da08ba22cfa936bf292980fd3d26f
Mika Ahopelto <mika.ahopelto@gmail.com>
2023-03-14 21:33:46 +0200

    Fixed responsiveness based on mobile first

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE = "Hackin' and Rollin'";
 export const SITE_DESCRIPTION = 'Thoughts, Ideas and Findings on a Journey to Solve Meaningful Challenges With the Help of Technology';
+export const ME = 'Mika Ahopelto'
 export const ABOUT_ME = 'Tech explorer, outdoor, bjj and family man. Continuously searching for meaningful questions and innovative solutions to them.'
